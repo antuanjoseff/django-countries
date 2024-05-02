f970c4b9d1c0aa7823fc2832550ee9a5a26c389f
antuanjoseff <toni.hernandez@udg.edu>
2024-05-02 16:22:52 +0200

    Fixtures data for countries. iso3, bbox and geom

--- countries/models.py
+++ countries/models.py
@@ -2,7 +2,6 @@ from django.contrib.gis.db import models
 from django.contrib.gis.db import models
 
 class Country(models.Model):
-    iso2 = models.CharField(max_length=2)
     iso3 = models.CharField(max_length=3, primary_key=True)
     name = models.CharField(max_length=200)
     geom = models.MultiPolygonField(spatial_index=False, null=True, blank=True)
@@ -18,7 +17,7 @@ class Country(models.Model):
 class Institution(models.Model):
     name = models.CharField(max_length=255, verbose_name='Nom')
     country = models.ForeignKey('Country', on_delete=models.CASCADE, verbose_name='País')
-    geom = models.PointField( spatial_index=False, verbose_name='Localització')
+    geom = models.PointField( spatial_index=False, verbose_name='Localització', null=True, blank=True)
     
     class Meta:
         verbose_name = 'Institució'
